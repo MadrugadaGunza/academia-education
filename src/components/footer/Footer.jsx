8bd9dba6751e03028057ce684c071079be56ae88
Madrugada de Carvalho <madrugadaoca@gmail.com>
2025-05-01 23:35:26 +0100

    feat: altera iframe de localização

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import './Footer.css'
+import { blog } from '../../dummydata';
 
 const Footer = () => {
     return (
@@ -15,6 +16,79 @@ const Footer = () => {
                     </div>
                 </div>
             </section>
+            <footer>
+                <div className="container padding">
+                    <div className="box logo">
+                        <h1>ACADEMIA</h1>
+                        <span>EDUCAÇÃO & APRENDIZAGEM ONLINE</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae debitis enim quo quas soluta cumque fuga.</p>
+                        <i className="fab fa-facebook-f icon"></i>
+                        <i className="fab fa-instagram icon"></i>
+                        <i className="fab fa-twitter icon"></i>
+                        <div className="box link">
+                            <h3>Explore</h3>
+                            <ul>
+                                <li>About US</li>
+                                <li>About US</li>
+                                <li>About US</li>
+                                <li>About US</li>
+                                <li>About US</li>
+                            </ul>
+                        </div>
+                        <div className="box link">
+                            <h3>Quick Links</h3>
+                            <ul>
+                                <li>About US</li>
+                                <li>About US</li>
+                                <li>About US</li>
+                                <li>About US</li>
+                                <li>About US</li>
+                            </ul>
+                        </div>
+                        <div className="box">
+                            <h3>Recent Posts</h3>
+                            {blog.splice(0, 3).map((val) => (
+                                <div className="items flexSB" key={val.id}>
+                                    <div className="img">
+                                        <img src={val.cover} alt={val.title} />
+                                    </div>
+                                    <div className="text">
+                                        <span>
+                                            <i className="fa fa-user"></i>
+                                            <label htmlFor="">{val.type}</label>
+                                        </span>
+                                        <span>
+                                            <i className="fa fa-calendar-alt"></i>
+                                            <label htmlFor="">{val.date}</label>
+                                        </span>
+                                        <h4>{val.title}</h4>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="box last">
+                            <h3>Have a questions?</h3>
+                            <ul>
+                                <li>
+                                    <i className="fa fa-map"></i>
+                                    203 Fake ST. Mount view, fan francisco USA
+                                </li>
+                                <li>
+                                    <i className="fa fa-phone-alt"></i>
+                                    +244 972 618 734
+                                </li>
+                                <li>
+                                    <i className="fa fa-paper-plane"></i>
+                                    madrugadaoca@gmail.com
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <div className="legal">
+                Copyright &copy; 2025 Todos os direitos reservados | Este projecto foi feito com <i className='fa fa-heart'></i> e React by Madrugada de Carvalho.
+            </div>
         </>
     )
 }
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,7 +2,8 @@ import './Contact.css'
 import Back from './../../components/back/Back';
 
 const Contact = () => {
-    const map = 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3941.7636840547857!2d13.192871824585886!3d-8.901548891154762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smorro%20bento!5e0!3m2!1spt-BR!2sao!4v1745953764260!5m2!1spt-BR!2sao'
+    //"https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3941.7636840547857!2d13.192871824585886!3d-8.901548891154762!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smorro%20bento!5e0!3m2!1spt-BR!2sao!4v1745953764260!5m2!1spt-BR!2sao"
+    const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3941.7508958992307!2d13.187620584184918!3d-8.902735627574945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1a51f4dec585e4a7%3A0x9b30267c56340bfc!2sCandando%20(Morro%20Bento)!5e0!3m2!1spt-BR!2sao!4v1746048905032!5m2!1spt-BR!2sao'
     // width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade
 
     return (
@@ -15,7 +16,7 @@ const Contact = () => {
                     </div>
                     <div className="right row">
                         <h1>Entre em Contacto</h1>
-                        <p>Aliquid iusto vel velit sint magni totam facilis facere?</p>
+                        {/* <span>Vamos trabalhar juntos?</span> */}
                         <div className="items grid2">
                             <div className="box">
                                 <h4>Endereço:</h4>
